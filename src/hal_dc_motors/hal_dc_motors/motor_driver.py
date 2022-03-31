eb2195b3e00dd133df9a0a59923fe8b7d299486e
projekt <robotsterowanygestami@gmail.com>
2022-03-31 18:19:31 +0200

    one motor support

--- src/hal_dc_motors/hal_dc_motors/motor_driver.py
+++ src/hal_dc_motors/hal_dc_motors/motor_driver.py
@@ -16,26 +16,28 @@ class MotorDriver(Node):
         #self.publisher_ = self.create_publisher(Int, 'topic', 10)
         self.subscriber_ = self.create_subscription(Int8, 'motor_speed', self.motor_speed_callback, 10)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(4, GPIO.OUT)
-        self.pwm = GPIO.PWM(4, 100)
+        self.PWM_PIN = 4
+        GPIO.setup(self.PWM_PIN, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.PWM_PIN, 100)
         self.speed = 0
         self.pwm.start(0)
-        self.Motor1A = 9
-        self.Motor1B = 10
-        GPIO.setup(self.Motor1A, GPIO.OUT)
-        GPIO.setup(self.Motor1B, GPIO.OUT)
+        self.MotorA = 9
+        self.MotorB = 10
+        GPIO.setup(self.MotorA, GPIO.OUT)
+        GPIO.setup(self.MotorB, GPIO.OUT)
 
     def motor_speed_callback(self, msg):
         self.speed = msg.data
         if self.speed >= 0:
-            GPIO.output(self.Motor1A, GPIO.HIGH)
-            GPIO.output(self.Motor1B, GPIO.LOW)
+            GPIO.output(self.MotorA, GPIO.HIGH)
+            GPIO.output(self.MotorB, GPIO.LOW)
+
         else:
-            GPIO.output(self.Motor1A, GPIO.LOW)
-            GPIO.output(self.Motor1B, GPIO.HIGH)
+            GPIO.output(self.MotorA, GPIO.LOW)
+            GPIO.output(self.MotorB, GPIO.HIGH)
             self.speed = -self.speed
         self.pwm.ChangeDutyCycle(self.speed)
-        GPIO.output(4, GPIO.HIGH)
+        GPIO.output(self.PWM_PIN, GPIO.HIGH)
 
 
 def main(args=None):
