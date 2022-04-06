19c7c2d1c3cbece51e1522c76708fd9295f1ab6c
projekt <robotsterowanygestami@gmail.com>
2022-04-06 17:02:48 +0200

    change name python script

--- src/hal_ultrasonic_sensors/hal_ultrasonic_sensors/hal_ultrasonic_sensor_driver.py
+++ src/hal_ultrasonic_sensors/hal_ultrasonic_sensors/hal_ultrasonic_sensor_driver.py
@@ -19,13 +19,14 @@ OUTPIN=17
 
 class Sensor(Node):
 
-    def __init__(self):
+    def __init__(self, args=None):
         super().__init__('hal_front_ultrasonic_sensor_driver')
         self.publisher_ = self.create_publisher(Float32, 'front_right_distance', 10)
         timer_period = 0.001 
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
         self.echo1=False
+        self.args=args
         gpio.setmode(gpio.BCM)
         gpio.setup(PIN1,gpio.IN)
         gpio.setup(PIN2,gpio.IN)
@@ -33,9 +34,11 @@ class Sensor(Node):
         gpio.setup(PIN4,gpio.IN)
         gpio.setup(OUTPIN,gpio.OUT)
         gpio.output(OUTPIN, gpio.LOW)
-        gpio.add_event_detect(PIN1,RISING,callback1)
+        gpio.add_event_detect(PIN1,gpio.RISING,self.callback1)
         self.start_time=None
         self.stop_time=None
+        self.start_flag=False
+        rclpy.init(args=self.args)
 
     def timer_callback(self):
         msg = Float32()
@@ -46,12 +49,19 @@ class Sensor(Node):
     def callback1(self):
         self.echo1=True
 
-    def run(self):
-        while rclpy.ok():
-            start_time=
-            gpio.output(OUTPIN,gpio.HIGH)
-            gpio.output(OUTPIN,gpio.LOW)
-            if self.echo1==True:
+    # def run(self):
+    #     while rclpy.ok():
+    #         if self.start_flag==False:
+    #             self.start_flag==True
+    #             self.start_time=time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+    #             gpio.output(OUTPIN,gpio.HIGH)
+    #             gpio.output(OUTPIN,gpio.LOW)
+    #         if self.echo1==True:
+    #             self.stop_time=time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+    #         if self.echo1==True:
+    #             rclpy.spin_once()
+    #     self.destroy
+
                 
 
 
@@ -60,12 +70,25 @@ class Sensor(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    sensor = Sensor()
 
     while rclpy.ok():
-        rclpy.spin_once(minimal_publisher)
+        if sensor.start_flag==False:
+            sensor.start_flag==True
+            sensor.start_time=time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+            gpio.output(OUTPIN,gpio.HIGH)
+            gpio.output(OUTPIN,gpio.LOW)
+        if sensor.echo1==True:
+            sensor.stop_time=time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+        #if sensor.echo1==True and sensor.start_flag==True:
+            msg = Float32()
+            msg.data = (sensor.stop_time-sensor.start_time)*17000
+            sensor.publisher_.publish(msg)
+            rclpy.spin_once(sensor)
+            sensor.echo1=False
+            sensor.start_flag=False
 
-    minimal_publisher.destroy_node()
+    sensor.destroy_node()
     rclpy.shutdown()
 
 
