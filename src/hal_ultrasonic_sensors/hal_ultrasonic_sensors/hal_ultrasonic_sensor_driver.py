fa0128ef4497743a7c8e904876fc9200f2d8b39b
projekt <robotsterowanygestami@gmail.com>
2022-04-07 12:17:34 +0200

    add two sensor driver

--- src/hal_ultrasonic_sensors/hal_ultrasonic_sensors/hal_ultrasonic_sensor_driver.py
+++ src/hal_ultrasonic_sensors/hal_ultrasonic_sensors/hal_ultrasonic_sensor_driver.py
@@ -1,105 +1,88 @@
-#!/usr/bin/env python3
-import RPi.GPIO as gpio
+#Libraries
+import RPi.GPIO as GPIO
 import time
-import sys
-import signal
-
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import Float32
-import RPi.GPIO as gpio
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 17
+GPIO_ECHO_1 = 5
+GPIO_ECHO_2 = 6
+ 
+#set GPIO direction (IN / OUT)
 
-PIN1=5
-PIN2=6
-PIN3=23
-PIN4=24
-OUTPIN=17
 
 
-class Sensor(Node):
-
+class UltrasoundSensorDriver(Node):
+    
     def __init__(self):
-        super().__init__('hal_ultrasonic_sensor_driver')
-        self.publisher_ = self.create_publisher(Float32, 'front_right_distance', 10)
-        # timer_period = 0.001 
-        # self.timer = self.create_timer(timer_period, self.timer_callback)
-        # self.i = 0
-        self.echo1=False
-        self.echo1_stop=False
-        gpio.setmode(gpio.BCM)
-        gpio.setup(PIN1,gpio.IN)
-        gpio.setup(PIN2,gpio.IN)
-        gpio.setup(PIN3,gpio.IN)
-        gpio.setup(PIN4,gpio.IN)
-        gpio.setup(OUTPIN,gpio.OUT)
-        gpio.output(OUTPIN, gpio.LOW)
-        gpio.add_event_detect(PIN1,gpio.RISING,self.callback1)
-        gpio.add_event_detect(PIN1,gpio.FALLING,self.callback1_off)
-        self.start_time=None
-        self.stop_time=None
-        self.start_flag=False
-
-    # def timer_callback(self):
-    #     msg = Float32()
-    #     msg.data = self.i
-    #     self.publisher_.publish(msg)
-    #     self.i += 1
-
-    def callback1(self,_):
-        self.get_logger().info("callback")
-        self.echo1=True
-
-    def callback1_off(self,_):
-        self.echo1_stop=True
-
-    # def run(self):
-    #     while rclpy.ok():
-    #         if self.start_flag==False:
-    #             self.start_flag==True
-    #             self.start_time=time.clock_gettime_ns(time.CLOCK_MONOTONIC)
-    #             gpio.output(OUTPIN,gpio.HIGH)
-    #             gpio.output(OUTPIN,gpio.LOW)
-    #         if self.echo1==True:
-    #             self.stop_time=time.clock_gettime_ns(time.CLOCK_MONOTONIC)
-    #         if self.echo1==True:
-    #             rclpy.spin_once()
-    #     self.destroy
-
-                
-
-
+        super().__init__('hal_ultrasound_sensor_driver')
+        self.front_right_publisher = self. create_publisher(Float32, 'front_right_distance', 10)
+        self.front_left_publisher = self.create_publisher(Float32, 'front_left_distance', 10)
+        GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO_1, GPIO.IN)
+        GPIO.setup(GPIO_ECHO_2, GPIO.IN)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.run)
+        self.i = 0
+        
+    def distance(self, trigger, echo):
+        # set Trigger to HIGH
+        GPIO.output(trigger, True)
+     
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(trigger, False)
+     
+        StartTime = time.time()
+        StopTime = time.time()
+     
+        # save StartTime
+        while GPIO.input(echo) == 0:
+            StartTime = time.time()
+     
+        # save time of arrival
+        while GPIO.input(echo) == 1:
+            StopTime = time.time()
+     
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+     
+        return distance
+    
+    def run(self):
+        msg = Float32()
+        msg.data = self.distance(GPIO_TRIGGER, GPIO_ECHO_1)
+        self.front_right_publisher.publish(msg)
+        self.get_logger().info('Publishing right: "%f"' % msg.data)
+        msg.data = self.distance(GPIO_TRIGGER, GPIO_ECHO_2)
+        self.front_left_publisher.publish(msg)
+        self.get_logger().info('Publishing left: "%f"' % msg.data)
+        
+        
 
 
 def main(args=None):
     rclpy.init(args=args)
-
-    sensor = Sensor()
-
+    
+    ultrasound_sensor_driver = UltrasoundSensorDriver()
+    
     while rclpy.ok():
-        if sensor.start_flag==False:
-            sensor.start_flag=True
-            gpio.output(OUTPIN,gpio.HIGH)
-            time.sleep(0.00001)
-            gpio.output(OUTPIN,gpio.LOW)
-        if sensor.echo1==True:
-            sensor.start_time=time.time_ns()
-        if sensor.echo1_stop==True:
-            sensor.stop_time=time.time_ns()
-        #if sensor.echo1==True and sensor.start_flag==True:
-            msg = Float32()
-            msg.data = float(((sensor.stop_time-sensor.start_time)/1000)*34/1000/2)
-            # msg.data=round(msg.data,3)
-            sensor.get_logger().info('Publishing: %f'% msg.data)
-            sensor.publisher_.publish(msg)
-            rclpy.spin_once(sensor)
-            sensor.echo1=False
-            sensor.start_flag=False
+        rclpy.spin_once(ultrasound_sensor_driver)
+        time.sleep(1)
 
-    sensor.destroy_node()
+        
+    ultrasound_sensor_driver.destron_node()
     rclpy.shutdown()
-
-
+    GPIO.cleanup()
+    
+ 
 if __name__ == '__main__':
     main()
-
