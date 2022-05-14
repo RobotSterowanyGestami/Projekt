81f8c7dc8e108a022f7dd90a2b4bed5b750d2b4b
projekt <robotsterowanygestami@gmail.com>
2022-05-14 12:01:10 +0200

    delete comments

--- src/hal_servo/hal_servo/hal_servo_driver.py
+++ src/hal_servo/hal_servo/hal_servo_driver.py
@@ -31,28 +31,6 @@ def main(args=None):
         
 if __name__ == '__main__':
     main()
-        
 
 
 
-
-#servoPIN = 12
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(servoPIN, GPIO.OUT)
-
-#p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-#p.start(0) # Initialization
-#try:
-#  while True:
-#    p.ChangeDutyCycle(1.5)
-#    time.sleep(1)
-#    p.ChangeDutyCycle(7)
-#    time.sleep(1)
-#    p.ChangeDutyCycle(12.5)
-#    time.sleep(1)
-#    p.ChangeDutyCycle(7.5)
-#    time.sleep(1)
-
-#except KeyboardInterrupt:
- # p.stop()
-  #GPIO.cleanup()
