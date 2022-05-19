07ba95d7529c62d7eb3851bc71d32a6f870e4f88
projekt <robotsterowanygestami@gmail.com>
2022-05-19 16:47:50 +0200

    shift to pigpio

--- src/hal_ultrasonic_sensors/hal_ultrasonic_sensors/hal_ultrasonic_sensor_driver.py
+++ src/hal_ultrasonic_sensors/hal_ultrasonic_sensors/hal_ultrasonic_sensor_driver.py
@@ -1,5 +1,5 @@
 #Libraries
-import RPi.GPIO as GPIO
+import pigpio
 import time
 import rclpy
 from rclpy.node import Node
@@ -23,28 +23,26 @@ class UltrasoundSensorDriver(Node):
         super().__init__('hal_ultrasound_sensor_driver')
         self.name = name
         self.publisher = self.create_publisher(Float32, self.name, 10)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(trigger, GPIO.OUT)
-        GPIO.setup(echo, GPIO.IN)
         self.trigger = trigger
         self.echo = echo
         self.timer_period = 0.25
         self.timer = self.create_timer(self.timer_period, self.run)
+        self.pi = pigpio.pi()
         
     def distance(self, trigger, echo):
-        GPIO.output(trigger, True)
+        self.pi.write(trigger, 1)
         time.sleep(0.00001)
-        GPIO.output(trigger, False)
+        self.pi.write(trigger, 0)
      
         StartTime = time.time()
         StopTime = time.time()
      
-        while GPIO.input(echo) == 0:
+        while self.pi.read(echo) == 0:
             if time.time() - StopTime > self.timer_period:
                 break
             StartTime = time.time()
      
-        while GPIO.input(echo) == 1:
+        while self.pi.read(echo) == 1:
             StopTime = time.time()
      
         TimeElapsed = StopTime - StartTime
@@ -59,7 +57,7 @@ class UltrasoundSensorDriver(Node):
         msg.data = self.distance(self.trigger, self.echo)
         #msg.data = 0.1
         self.publisher.publish(msg)
-        self.get_logger().info('%s' % self.name)
+        #self.get_logger().info('%s' % self.name)
 
 
 
