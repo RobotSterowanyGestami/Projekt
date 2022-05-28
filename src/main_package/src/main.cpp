9d265e78d5b686c3298509ef7c16e43f7538a14a
projekt <robotsterowanygestami@gmail.com>
2022-05-28 11:40:48 +0200

    add main_package

---
+++ src/main_package/src/main.cpp
@@ -0,0 +1,126 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <poll.h>
+#include <stdio.h>
+//#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int8.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+using namespace std::chrono_literals;
+using namespace std_msgs::msg;
+using namespace std;
+using namespace std::placeholders;
+using namespace rclcpp;
+
+template <typename T>
+class My_publisher{
+  public:
+ 	My_publisher( Node* n, char topic[] ){
+ 		pub = n->create_publisher<T>(topic,10);
+ 	}
+ 	
+ 	void publish(T msg){
+ 		pub->publish(msg);
+ 	}
+ 	
+ 	
+  private:
+  	typename Publisher<T>::SharedPtr pub;
+};
+
+template <typename T>
+class Subscriber{
+  public:
+ 	Subscriber( Node* n, char topic[] ){
+ 		sub = n->create_subscription<T>(
+			topic,
+		 	10,
+			bind(&Subscriber::callback, this, _1)
+		);
+ 	}
+ 	
+ 	void callback(const T &msg){
+ 		this->msg.data = msg.data;
+ 	}
+ 	
+ 	T get_msg(){
+ 		return msg;
+ 	}
+ 	
+ 	
+  private:
+  	typename Subscription<T>::SharedPtr sub;
+  	T msg;
+};
+
+class Main : public Node
+{
+  public:
+    Main(NodeOptions options)
+    : Node("main", options), count_(0)
+    {
+    	motor = new My_publisher<Int8>(this,"motor_speed");
+    	servo = new My_publisher<Int16>(this,"servo_angle");
+    	
+    	
+    	left_encoder = new Subscriber<Float32>(this,"left_encoder");
+		 	 
+		timer = this->create_wall_timer(
+      		500ms,
+      		bind(&Main::timer_callback, this)
+      		);
+    }
+
+  private:
+  	// Loop
+    void timer_callback()
+    {
+   		struct pollfd mypoll = { 0, POLLIN }; 
+   		
+    	if( poll(&mypoll,1,0) ){
+    		
+    		if( !scanf("%d,%d",&(motor_speed.data),&(this->my_servo_angle.data)) )
+				scanf(",%d",&(my_servo_angle.data) );
+		
+//    		scanf("%d",&(motor_speed.data));
+    		scanf("%*[^\n]");
+    		
+    		motor->publish(motor_speed);
+    		servo->publish(my_servo_angle);
+    	}
+    }
+    
+    Int8 motor_speed;
+    
+    size_t count_;
+    Int8 motor_speed;
+	Int16 my_servo_angle;
+    
+    TimerBase::SharedPtr timer;
+    
+    My_publisher<Int8>* motor;
+    My_publisher<Int16>* servo;
+    Subscriber<Float32>* left_encoder;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  rclcpp::executors::SingleThreadedExecutor exec;
+  
+  auto example_node = std::make_shared<Main>(options);
+  exec.add_node(example_node);
+  while(rclcpp::ok())
+    {
+      exec.spin_once();
+    }
+  
+  rclcpp::shutdown();
+  return 0;
+}
